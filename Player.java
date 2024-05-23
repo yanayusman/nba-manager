624f92916903831ce14b83a1986a62b253334fdd
Shaurah Sasha <shaurahsasha@gmail.com>
2024-05-23 14:55:01 +0800

    changed jlist to jtable

---
+++ Player.java
@@ -0,0 +1,213 @@
+import javax.swing.*;
+import javax.swing.border.TitledBorder;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.*;
+
+public class Player  extends JFrame {
+    final private Font font = new Font("Arial", Font.BOLD, 30);             // to customize font
+    final private Dimension dimension = new Dimension(150, 50);
+    final private Dimension maxSize = new Dimension(600, 300);
+    final private Dimension minSize = new Dimension(400, 100);
+
+    private JPanel navigationPanel, sidebarPanel, contentPanel;
+    private JTextField heightField, weightField, positionField;
+    private DefaultListModel<String> playersFoundListModel;
+    private JList<String> playersFoundList;
+
+    public Player(){
+        initialize();
+    }
+
+    public void initialize() {
+        setTitle("Player Dynamic Search");                                               //name for every page
+        setSize(1200, 800);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        /**
+         * NAVIGATION PANEL
+         */
+        navigationPanel = new JPanel();
+        navigationPanel.setBackground(Color.LIGHT_GRAY);
+        navigationPanel.setPreferredSize(new Dimension(1200, 50));
+
+        JLabel titleLabel = new JLabel("NBA Manager Game");
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 20));
+        navigationPanel.add(titleLabel);
+
+        /**
+         * SIDEBAR PANEL
+         */
+        sidebarPanel = new JPanel();
+        sidebarPanel.setLayout(new BoxLayout(sidebarPanel, BoxLayout.Y_AXIS));
+        sidebarPanel.setBackground(Color.DARK_GRAY);
+        sidebarPanel.setPreferredSize(new Dimension(200, 750));
+        String[] buttonLabels = {"HOME", "TEAM", "PLAYER", "JOURNEY", "CONTRACT", "INJURY"};
+
+        for (String label : buttonLabels) {
+            JButton button = new JButton(label);
+            button.setPreferredSize(dimension);
+            button.setMaximumSize(maxSize);
+            button.setMinimumSize(minSize);
+            button.setBackground(Color.LIGHT_GRAY);
+            button.setFont(new Font("Arial", Font.BOLD, 15));
+            button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent arg0) {
+                    handleSidebarButtonClick(label);
+                }
+            });
+            sidebarPanel.add(button);
+        }
+        
+        /**
+         * CONTENT PANEL
+         */
+        contentPanel = new JPanel();
+        contentPanel.setBackground(Color.WHITE);
+        contentPanel.setLayout(new FlowLayout());
+        contentPanel.setPreferredSize(new Dimension(50, 500));
+
+        /**
+         * INPUT PANEL
+         */
+        JPanel inputPanel = new JPanel();
+
+        //Height Search Input Bar   
+        JLabel heightLabel = new JLabel("Height: ");
+        heightLabel.setFont(new Font("Arial", Font.PLAIN, 17));
+        inputPanel.add(heightLabel);
+        heightField = new JTextField();
+        heightField.setPreferredSize(new Dimension(100, 30));
+        heightField.setFont(new Font("Arial", Font.PLAIN, 17));
+        inputPanel.add(heightField);
+
+        //Weight Search Input Bar
+        JLabel weightLabel = new JLabel("Weight: ");
+        weightLabel.setFont(new Font("Arial", Font.PLAIN, 17));
+        inputPanel.add(weightLabel);
+        weightField = new JTextField();
+        weightField.setPreferredSize(new Dimension(100, 30));
+        weightField.setFont(new Font("Arial", Font.PLAIN, 17));
+        inputPanel.add(weightField);
+
+        //Position Search Input Bar
+        JLabel positionLabel = new JLabel("Position: ");
+        positionLabel.setFont(new Font("Arial", Font.PLAIN, 17));
+        inputPanel.add(positionLabel);
+        positionField = new JTextField();
+        positionField.setPreferredSize(new Dimension(100, 30));
+        positionField.setFont(new Font("Arial", Font.PLAIN, 17));
+        inputPanel.add(positionField);
+
+        //Search Button
+        JButton searchButton = new JButton("SEARCH");
+        searchButton.setFont(new Font("Arial", Font.BOLD, 17));
+        searchButton.setPreferredSize(new Dimension(125, 45));
+        //addButton.addActionListener();
+
+        JPanel searchButtonPanel = new JPanel();
+        searchButtonPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        searchButtonPanel.add(searchButton);
+        inputPanel.add(searchButtonPanel, BorderLayout.SOUTH);
+
+        /**
+         * LIST PANEL
+         */
+        JPanel listPanel = new JPanel(new BorderLayout());
+
+        //JList
+        playersFoundListModel = new DefaultListModel<>();
+        playersFoundList = new JList<>(playersFoundListModel);
+        playersFoundList.setPreferredSize(new Dimension(200, 200));
+        JPanel playersFoundPanel = new JPanel(new BorderLayout());
+        playersFoundPanel.setBorder(new TitledBorder("Match(es) Found: "));
+        playersFoundPanel.add(new JScrollPane(playersFoundList), BorderLayout.CENTER);
+        loadPlayersFound();
+        listPanel.add(playersFoundPanel);
+
+
+        /**
+         * BOTTOM PANEL
+         */
+        JPanel bottomPanel = new JPanel(new BorderLayout());
+
+        //Add to Team Button
+        JButton addButton = new JButton("ADD TO TEAM");
+        addButton.setFont(new Font("Arial", Font.BOLD, 17));
+        addButton.setPreferredSize(new Dimension(150, 45));
+        //addButton.addActionListener();
+
+        JPanel addButtonPanel = new JPanel();
+        addButtonPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        addButtonPanel.add(addButton);
+        bottomPanel.add(addButtonPanel, BorderLayout.SOUTH);
+
+        contentPanel.add(inputPanel, BorderLayout.NORTH);
+        contentPanel.add(listPanel, BorderLayout.CENTER);
+        contentPanel.add(bottomPanel, BorderLayout.SOUTH);
+
+        getContentPane().setLayout(new BorderLayout());
+        getContentPane().add(navigationPanel, BorderLayout.NORTH);
+        getContentPane().add(sidebarPanel, BorderLayout.WEST);
+        getContentPane().add(contentPanel, BorderLayout.CENTER);                // set content position
+   
+        setVisible(true);
+    }
+
+    private void loadPlayersFound() {
+        String jdbcUrl = "jdbc:mysql://localhost:3306/nbamanager";
+        String username = "useract";
+        String password = "welcome1";
+
+        String height = heightField.getText();
+        String weight = weightField.getText() + " lbs";
+        String position = positionField.getText();
+
+        try (Connection con = DriverManager.getConnection(jdbcUrl, username, password)) {
+            String sql = "SELECT * FROM players_profile WHERE height LIKE " + height + 
+                            " OR weight LIKE " + weight + " AND position LIKE " + position;
+            Statement statement = con.createStatement();
+            ResultSet rs = statement.executeQuery(sql);
+
+            while (rs.next()) {
+                String playername = rs.getString("name");
+                playersFoundListModel.addElement(playername);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void handleSidebarButtonClick(String label) {
+        switch (label) {
+            case "HOME":
+                new Home();
+                break;
+            case "TEAM":
+                new Temp();
+                break;
+            case "PLAYER":
+                new Temp(); 
+                break;
+            case "JOURNEY":
+                new Temp();
+                break;
+            case "CONTRACT":
+                new Contract();
+                break;
+            case "INJURY":
+                new Injury();
+                break;
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new Player();
+        });
+    }
+}
