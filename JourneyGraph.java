1a4aba2f96227c1b5f3abc233cbd39f7b7cf3259
yanayusman <anastasia.allegra@gmail.com>
2024-06-05 21:51:11 +0800

    updated latest JourneyGraph.java

--- JourneyGraph.java
+++ JourneyGraph.java
@@ -1,3 +1,4 @@
+
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
@@ -28,11 +29,11 @@ public class JourneyGraph extends JFrame {
     private JTable routeTable;
     private DefaultTableModel tableModel;
     private RouteDistance<String, Integer> routeDistance;
-
+    
     public JourneyGraph() {
         initialize();
     }
-
+    
     private void initialize() {
         setTitle("Journey Plan");                                               //name for every page
         setSize(1200, 800);
@@ -76,14 +77,12 @@ public class JourneyGraph extends JFrame {
         contentPanel.setBackground(Color.WHITE);
         contentPanel.setOpaque(false);
         contentPanel.setLayout(new BorderLayout());
-        //contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
-        //contentPanel.setPreferredSize(new Dimension(50, 500));
-
+      
         //map image panel
-        JLabel imageLabel = loadImage("NBAMap.png", true,450, 260);
+        JLabel imageLabel = loadImage("NBAMap.jpg", true,450, 260);
         JPanel mapPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
         mapPanel.add(imageLabel);
-
+        
         routeDistance = new RouteDistance<>(JourneyRoutes()); // Assuming map1 is the graph used for route calculation
         String startCity = "San Antonio(Spurs)";
 
@@ -99,11 +98,13 @@ public class JourneyGraph extends JFrame {
         //BEST ROUTE DISPLAY
         JPanel bestRoutePanel = new JPanel();
         bestRoutePanel.setLayout(new BoxLayout(bestRoutePanel, BoxLayout.Y_AXIS));
-
+        
         JLabel bestRouteLabel = new JLabel("Best Route:");
         bestRouteLabel.setFont(new Font("Arial", Font.BOLD, 15));
-
-if(!dfsRoutes.isEmpty()){
+        //bestRoutePanel.add(bestRouteLabel);
+       
+        //arrange so that the lowest distances at front array
+       if(!dfsRoutes.isEmpty()){
         int firstTotalDistance = totalDistance[0];
         for (ArrayList<String> route : dfsRoutes){
             if(firstTotalDistance==calculateTotalDistance(route, JourneyRoutes())){            
@@ -124,21 +125,18 @@ if(!dfsRoutes.isEmpty()){
             bestRoutePanel.add(firstRouteLabel); 
 
             JFrame bestMapFrame = new JFrame("Best Route Map");
-            //JLabel bestMapImage = loadImage("2024-05-25 (2).png", true,400, 250);
-            //bestMapFrame.add(bestMapImage);
-
+        
             JButton bestButton = new JButton("View Map");
             bestButton.addActionListener(e->showPopup(bestMapFrame,0,firstFormattedRoute));
-
+            
             bestRoutePanel.add(bestButton);
         }
     }}
         JPanel otherRoutesPanel = new JPanel();
         otherRoutesPanel.setLayout(new BoxLayout(otherRoutesPanel,1));
-
+        
         JLabel otherRouteLabel = new JLabel("Other Routes:");
         otherRouteLabel.setFont(new Font("Arial", Font.BOLD, 15));
-        //otherRoutesPanel.add(otherRouteLabel);
 
         i=0;
         for (ArrayList<String> route : dfsRoutes) {
@@ -164,181 +162,72 @@ if(!dfsRoutes.isEmpty()){
                 routeLabel.setText("<html>" + (i+1) +". " + formattedRoute.toString() + "<br>" +"Distance: "+ totalDistance[i] + " km"+"</html>"); // Use HTML to create multiline label
                 routeLabel.setFont(font);
                 otherRoutesPanel.add(routeLabel); 
-
+                
                 JFrame MapFrame = new JFrame("Route "+(i+1) +" Map");
-                //JLabel bestMapImage = loadImage("2024-05-25 (2).png", true,400, 250);
-                //bestMapFrame.add(bestMapImage);
-
+         
                 JButton Button = new JButton("View Map");
                 int routeIndex =i;
                 Button.addActionListener(e->showPopup(MapFrame,routeIndex,formattedRoute));
-
+                
                 otherRoutesPanel.add(Button);
-                //tableModel.addRow(new Object[]{routeLabel, totalDistance[i]});
             }
         }
     }
-
-        // Call DFS
-        //ArrayList<ArrayList<String>> dfsRoutes = routeDistance.depthFirstSearch(startCity);
-
-        /*JPanel dfspanel = new JPanel();
-        dfspanel.setLayout(new BorderLayout());
-        JLabel totalDistanceLabel =new JLabel();
-        
-        int journeyNumber=1;
-        dfspanel.setLayout(new BoxLayout(dfspanel, BoxLayout.Y_AXIS));
-        JLabel dfslabel = new JLabel("Depth First Search Method:");
-        dfslabel.setFont(new Font("Arial", Font.BOLD, 15));
-        dfslabel.setPreferredSize(new Dimension(200, 30)); // Adjust dimensions as needed
-        dfspanel.add(dfslabel);
-        if (!dfsRoutes.isEmpty()) {
-            for (ArrayList<String> journey : dfsRoutes) {
-                JPanel journeyRoutePanel1 = new JPanel(new GridLayout(2, 1));
-                totalDistanceLabel = new JLabel();
-                StringBuilder journeyString = new StringBuilder("Journey " + journeyNumber + ": ");
-                for (String city : journey) {
-                    journeyString.append(city).append(" -> ");
-                }
-                journeyString.setLength(journeyString.length() - 4); // Remove the last " -> "
-                JLabel journeyLabel = new JLabel(journeyString.toString());
-                journeyRoutePanel1.add(journeyLabel, BorderLayout.NORTH);
-                int totalDistance = calculateTotalDistance(journey, JourneyRoutes());
-                totalDistanceLabel = new JLabel("Total Distance: " + totalDistance);
-                journeyRoutePanel1.add(totalDistanceLabel, BorderLayout.CENTER);
-                dfspanel.add(journeyRoutePanel1);
-                journeyNumber++;
-            }
-        } else {
-            totalDistanceLabel = new JLabel("No journey consisting of all cities found.");
-            dfspanel.add(totalDistanceLabel, BorderLayout.CENTER);
-        }
-        JScrollPane scrollPane = new JScrollPane(dfspanel);
-        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-        /* 
-        //Call BFS
-        ArrayList<ArrayList<String>> bfsRoutes = routeDistance.breadthFirstSearch(startCity);
-        JPanel bfspanel = new JPanel();
-        bfspanel.setLayout(new BorderLayout());
-        
-        JLabel bfslabel = new JLabel("Breadth First Search Method:");
-        bfslabel.setFont(new Font("Arial", Font.BOLD, 15));
-        bfspanel.add(bfslabel);
-        journeyNumber = 1;
-        JPanel journeyRoutePanel2 = new JPanel(new BorderLayout());
-        if(!bfsRoutes.isEmpty()){
-            for (ArrayList<String> journey : bfsRoutes) {
-                journeyRoutePanel2 = new JPanel(new BorderLayout());
-                StringBuilder journeyString = new StringBuilder("Journey " + journeyNumber + ": ");
-                for (String city : journey) {
-                    journeyString.append(city).append(" -> ");
-                }
-                journeyString.setLength(journeyString.length() - 4);
-                JLabel journeyLabel = new JLabel(journeyString.toString());
-                journeyRoutePanel2.add(journeyLabel, BorderLayout.NORTH);
-                int totalDistance = calculateTotalDistance(journey, JourneyRoutes());
-                totalDistanceLabel = new JLabel("Total Distance: " + totalDistance);
-                journeyRoutePanel2.add(totalDistanceLabel, BorderLayout.CENTER);
-                bfspanel.add(journeyRoutePanel2); 
-                journeyNumber++;
-                }
-        }else{
-            journeyRoutePanel2 = new JPanel(new BorderLayout());
-            totalDistanceLabel = new JLabel("No journey consisting of all cities found.");
-            journeyRoutePanel2.add(totalDistanceLabel, BorderLayout.CENTER);
-            bfspanel.add(journeyRoutePanel2);
-        }
-        ArrayList<String> bfsTraversal = routeDistance.bfsTraversal(startCity);
-        StringBuilder journeyString = new StringBuilder("Journey :"+(journeyNumber-1) +":");
-        for (int i = 0; i < bfsTraversal.size(); i++) {
-            journeyString.append(bfsTraversal.get(i)).append(" -> ");
-        }
-            journeyString.setLength(journeyString.length() - 4);
-            JLabel journeyLabel = new JLabel(journeyString.toString());
-            journeyRoutePanel2.add(journeyLabel, BorderLayout.NORTH);
-            bfspanel.add(journeyRoutePanel2);*/
-
-        // Calculate total distance
-        //int totalDistance = calculateTotalDistance(bfsTraversal, map1);
-
-        // Print total distance
-        //System.out.println("Total Distance: " + totalDistance);
-
-        //JPanel journeyPanel = new JPanel(new GridLayout(2, 1, 10, 5));
-        //journeyPanel.setBorder(new TitledBorder("Journey Distances"));
-        //journeyPanel.add(bfspanel);
-        //journeyPanel.add(scrollPane);
-
         contentPanel.add(mapPanel,BorderLayout.NORTH);
-
+       
         JPanel bestRouteWrapperPanel = createBorderedPanel("B E S T  R O U T E : ");
         bestRouteWrapperPanel.add(bestRoutePanel, BorderLayout.CENTER);
-
-        //new JPanel(new BorderLayout());
-//bestRouteWrapperPanel.setBorder(new TitledBorder(new LineBorder(Color.BLACK), "Best Route"));
-//bestRouteWrapperPanel.add(bestRoutePanel, BorderLayout.CENTER);
-
-// OTHER ROUTES DISPLAY
+      
 JPanel otherRoutesWrapperPanel = createBorderedPanel("O T H E R  R O U T E S : ");
         otherRoutesWrapperPanel.add(otherRoutesPanel, BorderLayout.CENTER);
-//JPanel otherRoutesWrapperPanel = new JPanel(new BorderLayout());
-//otherRoutesWrapperPanel.setBorder(new TitledBorder(new LineBorder(Color.BLACK), "Other Routes"));
-//otherRoutesWrapperPanel.add(otherRoutesPanel, BorderLayout.CENTER);
 
-        //bestRoutePanel.setAlignmentX(Component.LEFT_ALIGNMENT);
-        //otherRoutesPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
-
+         
         JPanel routePanels = new JPanel();
         routePanels.setLayout(new BoxLayout(routePanels, BoxLayout.X_AXIS));;
-        //contentPanel.add(bestRoutePanel);
-        //contentPanel.add(otherRoutesPanel);
-        //contentPanel.add(dfsPanel);
-        //contentPanel.add(scrollPane, BorderLayout.SOUTH);
         routePanels.add(bestRouteWrapperPanel);
         routePanels.add(otherRoutesWrapperPanel);
 
         contentPanel.add(routePanels);
-        //contentPanel.add(otherRoutesWrapperPanel);
 
         getContentPane().setLayout(new BorderLayout());
         getContentPane().add(navigationPanel, BorderLayout.NORTH);
         getContentPane().add(sidebarPanel, BorderLayout.WEST);
         getContentPane().add(contentPanel, BorderLayout.CENTER);  
-
+        
     }
 
     private JPanel createBorderedPanel(String title) {
     JPanel panel = new JPanel();
     panel.setLayout(new BorderLayout());
-
+    
     // Create an empty border with padding around the panel
     EmptyBorder border = new EmptyBorder(10, 10, 10, 10); // Adjust padding as needed
     panel.setBorder(border);
-
+    
     // Add a titled border to the panel
     TitledBorder titledBorder = new TitledBorder(title);
     titledBorder.setTitlePosition(TitledBorder.DEFAULT_POSITION);
     panel.setBorder(BorderFactory.createCompoundBorder(titledBorder, BorderFactory.createEmptyBorder(5, 5, 5, 5))); // Adjust padding as needed
-
+    
     return panel;
 }
     private void showPopup(JFrame frame, int a, StringBuilder b){
         String imageName="";
         switch(a){
             case(0):
-                imageName = "bestRouteMap.png";
+                imageName = "bestRouteMap.jpg";
                 break;
             case(1):
-                imageName = "routeMap2.png";
+                imageName = "routeMap2.jpg";
                 break;
             case(2):
-                imageName = "routeMap3.png";
+                imageName = "routeMap3.jpg";
                 break;
             case(3):
-                imageName = "routeMap4.png";
+                imageName = "routeMap4.jpg";
                 break;
             case(4):
-                imageName = "routeMap5.png";
+                imageName = "routeMap5.jpg";
                 break;
         }
         JLabel bestMapImage = loadImage(imageName, true,400, 250);
@@ -355,15 +244,10 @@ JPanel otherRoutesWrapperPanel = createBorderedPanel("O T H E R  R O U T E S : "
         routeLabel.setFont(new Font("Monospace", Font.BOLD, 14));
         panel.add(routeLabel);
         panel.add(bestMapImage);
-       // panel.add(backButton, BorderLayout.SOUTH);
+      
 
         JOptionPane.showMessageDialog(frame, panel, "Best Route Map", JOptionPane.PLAIN_MESSAGE);
-
-        // Add action listener to the back button
-        //backButton.addActionListener(e -> {
-            // Handle back button click here
-            //JOptionPane.getRootFrame().dispose(); // Close the popup
-        //});
+      
     }
 
     private static int calculateTotalDistance(ArrayList<String> journey, Map<String, Integer> map) {
@@ -375,7 +259,7 @@ JPanel otherRoutesWrapperPanel = createBorderedPanel("O T H E R  R O U T E S : "
         }
         return totalDistance;
     }
-
+    
     private void handleSidebarButtonClick(String label) {
         this.dispose();
         switch (label) {
@@ -386,7 +270,7 @@ JPanel otherRoutesWrapperPanel = createBorderedPanel("O T H E R  R O U T E S : "
                 new MyTeam();
                 break;
             case "PLAYER":
-                new Player_(); 
+                new MyPlayer(); 
                 break;
             case "JOURNEY":
                 new JourneyGraph();
@@ -428,7 +312,7 @@ JPanel otherRoutesWrapperPanel = createBorderedPanel("O T H E R  R O U T E S : "
         graphics2D.dispose();
         return resizedImg;
     }
-
+    
     public Map<String, Integer> JourneyRoutes(){
         Map<String,Integer> map1 = new Map<>();
         String[] cities = {"San Antonio(Spurs)","Golden State(Warriors)","Boston(Celtics)",
@@ -436,32 +320,32 @@ JPanel otherRoutesWrapperPanel = createBorderedPanel("O T H E R  R O U T E S : "
             "Denver(Nuggets)","Oklahoma City(Thunder)","Houston(Rockets)"};
         for(String i:cities)
             map1.addCity(i);
-
+        
         System.out.println("Number of cities: "+map1.getSize());
-
+        
         map1.addRoute("San Antonio(Spurs)","Phoenix(Suns)", 500);
         map1.addRoute("San Antonio(Spurs)","Oklahoma City(Thunder)", 678);
         map1.addRoute("San Antonio(Spurs)","Houston(Rockets)", 983);
         map1.addRoute("San Antonio(Spurs)","Orlando(Magic)", 1137);
-
+        
         map1.addRoute("Phoenix(Suns)","Los Angeles(Lakers)", 577);
-
+        
         map1.addRoute("Los Angeles(Lakers)","Oklahoma City(Thunder)", 1901);
         map1.addRoute("Los Angeles(Lakers)","Golden State(Warriors)", 554);
-
+        
         map1.addRoute("Golden State(Warriors)","Oklahoma City(Thunder)", 2214);
         map1.addRoute("Golden State(Warriors)","Denver(Nuggets)", 1507);
-
+        
         map1.addRoute("Denver(Nuggets)","Oklahoma City(Thunder)", 942);
         map1.addRoute("Denver(Nuggets)","Boston(Celtics)", 2845);
-
+        
         map1.addRoute("Boston(Celtics)","Houston(Rockets)", 2584);
         map1.addRoute("Boston(Celtics)","Miami(Heat)", 3045);
-
+        
         map1.addRoute("Miami(Heat)","Orlando(Magic)", 268);
-
+        
         map1.addRoute("Orlando(Magic)","Houston(Rockets)", 458);
-
+        
         map1.addRoute("Houston(Rockets)","Oklahoma City(Thunder)", 778);
 
         map1.printRoutes();
@@ -476,7 +360,7 @@ class City <T extends Comparable<T>, N extends Comparable<N>>{
     int outdeg;
     City<T,N> nextCity;
     Route<T,N> firstRoute;
-
+    
     public City(){
         cityName = null;
         indeg=0;
@@ -496,7 +380,7 @@ class Route<T extends Comparable<T>, N extends Comparable<N>>{
     City<T,N> toCity;
     N weight;
     Route<T,N> nextRoute;
-
+    
     public Route(){
         toCity=null;
         weight=null;
@@ -608,7 +492,7 @@ class Map<T extends Comparable<T>, N extends Comparable<N>> {
                 }
             }temp=temp.nextCity;
         }return list;
-
+        
     }
     public N getRouteWeight(T source, T destination){
         N notFound=null;
