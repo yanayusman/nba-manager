fd4680c628fe40e0cffe0ed12395d8ea7c50a6d8
yanayusman <anastasia.allegra@gmail.com>
2024-06-09 04:14:10 +0800

    updated player for ranking

--- Player.java
+++ Player.java
@@ -10,11 +10,16 @@ public class Player {
     public String country;
     public String label;
     public String salary;
-    public double avgPoints;
-    public double rebounds;
-    public double steals;
-    public double assists;
-    public double blocks;
+    public double pts;
+    public double oreb;
+    public double dreb;
+    public double reb;
+    public double stl;
+    public double ast;
+    public double blk;
+    public double tov;
+    public double fg;
+    public double eff;
     public double compositeScore;
     public String imagePath;
 
